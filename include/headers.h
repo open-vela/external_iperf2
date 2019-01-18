608fdf34e67b8802faa482537bc00b74618cad24
openvela-robot <openvela-robot@xiaomi.com>
2019-01-18 13:12:19 +0800

    more on man page

--- include/headers.h
+++ include/headers.h
@@ -232,40 +232,7 @@ typedef struct sockaddr_in iperf_sockaddr;
     #endif
 //#endif
 
-// Rationalize stdint definitions and sizeof, thanks to ac_create_stdint_h.m4
-// from the gnu archive
-
-#include <iperf-int.h>
-// Override <stdint.h> PRIdMAX (hack for now, fix this to use <stdint.h> properly)
-#ifdef HAVE_QUAD_SUPPORT
-  #ifdef WIN32
-    #define IPERFdMAX "I64d"
-  #elif defined HAVE_PRINTF_QD
-    #define IPERFdMAX "qd"
-  #else
-    #define IPERFdMAX "lld"
-  #endif
-#else
-  #define IPERFdMAX "d"
-#endif
-
-#ifdef HAVE_QUAD_SUPPORT
-#  ifdef HAVE_INT64_T
-typedef int64_t max_size_t;
-typedef u_int64_t umax_size_t;
-#  else
-typedef long long max_size_t;
-typedef unsigned long long umax_size_t;
-#  endif // INT64
-#else
-#  ifdef HAVE_INT32_T
-typedef int32_t max_size_t;
-typedef u_int32_t umax_size_t;
-#  else
-typedef long max_size_t;
-typedef unsigned long umax_size_t;
-#  endif // INT32
-#endif
+// inttypes.h is already included
 
 #ifdef HAVE_FASTSAMPLING
 #define IPERFTimeFrmt "%4.4f-%4.4f"
@@ -285,5 +252,10 @@ typedef unsigned long umax_size_t;
     #define SHUT_WR   1
     #define SHUT_RDWR 2
 #endif // SHUT_RD
+
+
+/* Internal debug */
+//#define INITIAL_PACKETID 0x7FFFFF00LL
+//#define SHOW_PACKETID
 
 #endif /* HEADERS_H */
