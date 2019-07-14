a2aed4ae56eda83ee30cdb06774e42a5fe781977
openvela-robot <openvela-robot@xiaomi.com>
2019-07-14 18:07:25 +0800

    Merge branch 'master' of ssh://git.code.sf.net/p/iperf2/code

--- src/PerfSocket.cpp
+++ src/PerfSocket.cpp
@@ -163,12 +163,6 @@ void SetSocketOptions( thread_Settings *inSettings ) {
 #endif
 
     if ( !isUDP( inSettings ) ) {
-        // set so linger
-        struct linger ling;
-        ling.l_onoff  = 1;
-        ling.l_linger = 5;     /* timeout is seconds */
-        setsockopt( inSettings->mSock, SOL_SOCKET, SO_LINGER, (char *)&ling, sizeof(struct linger) );
-
         // set the TCP maximum segment size
         setsock_tcp_mss( inSettings->mSock, inSettings->mMSS );
 
