4b43908143ac5efa3c886f5a905d307ec0875c02
openvela-robot <openvela-robot@xiaomi.com>
2019-07-30 23:52:25 +0800

    add support for --bidir command line option

--- include/Settings.hpp
+++ include/Settings.hpp
@@ -296,7 +296,7 @@ typedef struct thread_Settings {
 #define FLAG_MODEINFINITE   0x00010000
 #define FLAG_CONNECTONLY    0x00020000
 #define FLAG_SERVERREVERSE  0x00040000
-#define FLAG_WRITEACK      0x00080000
+#define FLAG_BIDIR          0x00080000
 
 #define isBuflenSet(settings)      ((settings->flags & FLAG_BUFLENSET) != 0)
 #define isCompat(settings)         ((settings->flags & FLAG_COMPAT) != 0)
@@ -331,20 +331,20 @@ typedef struct thread_Settings {
 #define isPeerVerDetect(settings)  ((settings->flags_extend & FLAG_PEERVER) != 0)
 #define isSeqNo64b(settings)       ((settings->flags_extend & FLAG_SEQNO64) != 0)
 #define isReverse(settings)        ((settings->flags_extend & FLAG_REVERSE) != 0)
+#define isBidir(settings)        ((settings->flags_extend & FLAG_BIDIR) != 0)
 #define isServerReverse(settings)  ((settings->flags_extend & FLAG_SERVERREVERSE) != 0)
 #define isIsochronous(settings)    ((settings->flags_extend & FLAG_ISOCHRONOUS) != 0)
-#define isRxHistogram(settings)    ((settings->flags_extend & FLAG_RXHISTOGRAM) != 0)
+#define isRxHistogram(settings)   ((settings->flags_extend & FLAG_RXHISTOGRAM) != 0)
 #define isL2LengthCheck(settings)  ((settings->flags_extend & FLAG_L2LENGTHCHECK) != 0)
 #define isIncrDstIP(settings)      ((settings->flags_extend & FLAG_INCRDSTIP) != 0)
-#define isTxStartTime(settings)    ((settings->flags_extend & FLAG_TXSTARTTIME) != 0)
+#define isTxStartTime(settings)         ((settings->flags_extend & FLAG_TXSTARTTIME) != 0)
 #define isTxHoldback(settings)     ((settings->flags_extend & FLAG_TXHOLDBACK) != 0)
 #define isVaryLoad(settings)       ((settings->flags_extend & FLAG_VARYLOAD) != 0)
 #define isFQPacing(settings)       ((settings->flags_extend & FLAG_FQPACING) != 0)
 #define isTripTime(settings)       ((settings->flags_extend & FLAG_TRIPTIME) != 0)
-#define isModeInfinite(settings)   (settings->flags_extend & FLAG_MODEINFINITE) != 0)
+#define isModeInfinite(settings)     ((settings->flags_extend & FLAG_MODEINFINITE) != 0)
 #define isModeAmount(settings)     (!isModeTime(settings) && !isModeInfinite(settings))
-#define isConnectOnly(settings)    ((settings->flags_extend & FLAG_CONNECTONLY) != 0)
-#define isWriteAck(settings)       ((settings->flags_extend & FLAG_WRITEACK) != 0)
+#define isConnectOnly(settings) ((settings->flags_extend & FLAG_CONNECTONLY) != 0)
 
 #define setBuflenSet(settings)     settings->flags |= FLAG_BUFLENSET
 #define setCompat(settings)        settings->flags |= FLAG_COMPAT
@@ -377,19 +377,19 @@ typedef struct thread_Settings {
 #define setPeerVerDetect(settings) settings->flags_extend |= FLAG_PEERVER
 #define setSeqNo64b(settings)      settings->flags_extend |= FLAG_SEQNO64
 #define setReverse(settings)       settings->flags_extend |= FLAG_REVERSE
+#define setBidir(settings)         settings->flags_extend |= FLAG_BIDIR
 #define setServerReverse(settings) settings->flags_extend |= FLAG_SERVERREVERSE
 #define setIsochronous(settings)   settings->flags_extend |= FLAG_ISOCHRONOUS
-#define setRxHistogram(settings)   settings->flags_extend |= FLAG_RXHISTOGRAM
-#define setL2LengthCheck(settings) settings->flags_extend |= FLAG_L2LENGTHCHECK
+#define setRxHistogram(settings)  settings->flags_extend |= FLAG_RXHISTOGRAM
+#define setL2LengthCheck(settings)    settings->flags_extend |= FLAG_L2LENGTHCHECK
 #define setIncrDstIP(settings)     settings->flags_extend |= FLAG_INCRDSTIP
-#define setTxStartTime(settings)   settings->flags_extend |= FLAG_TXSTARTTIME
+#define setTxStartTime(settings)        settings->flags_extend |= FLAG_TXSTARTTIME
 #define setTxHoldback(settings)    settings->flags_extend |= FLAG_TXHOLDBACK
 #define setVaryLoad(settings)      settings->flags_extend |= FLAG_VARYLOAD
 #define setFQPacing(settings)      settings->flags_extend |= FLAG_FQPACING
 #define setTripTime(settings)      settings->flags_extend |= FLAG_TRIPTIME
 #define setModeInfinite(settings)  settings->flags_extend |= FLAG_MODEINFINITE
 #define setConnectOnly(settings)   settings->flags_extend |= FLAG_CONNECTONLY
-#define setWriteAck(settings)      settings->flags_extend |= FLAG_WRITEACK
 
 #define unsetBuflenSet(settings)   settings->flags &= ~FLAG_BUFLENSET
 #define unsetCompat(settings)      settings->flags &= ~FLAG_COMPAT
@@ -420,21 +420,21 @@ typedef struct thread_Settings {
 #define unsetEnhanced(settings)    settings->flags &= ~FLAG_ENHANCEDREPORT
 #define unsetServerModeTime(settings)    settings->flags &= ~FLAG_SERVERMODETIME
 #define unsetPeerVerDetect(settings)    settings->flags_extend &= ~FLAG_PEERVER
-#define unsetSeqNo64b(settings)     settings->flags_extend &= ~FLAG_SEQNO64
-#define unsetReverse(settings)      settings->flags_extend &= ~FLAG_REVERSE
+#define unsetSeqNo64b(settings)    settings->flags_extend &= ~FLAG_SEQNO64
+#define unsetReverse(settings)     settings->flags_extend &= ~FLAG_REVERSE
+#define unsetBidir(settings)       settings->flags_extend &= ~FLAG_BIDIR
 #define unsetServerReverse(settings) settings->flags_extend &= ~FLAG_SERVERREVERSE
-#define unsetIsochronous(settings)  settings->flags_extend &= ~FLAG_ISOCHRONOUS
+#define unsetIsochronous(settings) settings->flags_extend &= ~FLAG_ISOCHRONOUS
 #define unsetRxHistogram(settings)    settings->flags_extend &= ~FLAG_RXHISTOGRAM
 #define unsetL2LengthCheck(settings)  settings->flags_extend &= ~FLAG_L2LENGTHCHECK
-#define unsetIncrDstIP(settings)    settings->flags_extend &= ~FLAG_INCRDSTIP
-#define unsetTxStartTime(settings)  settings->flags_extend &= ~FLAG_TXSTARTTIME
-#define unsetTxHoldback(settings)   settings->flags_extend &= ~FLAG_TXHOLDBACK
+#define unsetIncrDstIP(settings)   settings->flags_extend &= ~FLAG_INCRDSTIP
+#define unsetTxStartTime(settings) settings->flags_extend &= ~FLAG_TXSTARTTIME
+#define unsetTxHoldback(settings)  settings->flags_extend &= ~FLAG_TXHOLDBACK
 #define unsetVaryLoad(settings)     settings->flags_extend &= ~FLAG_VARYLOAD
 #define unsetFQPacing(settings)     settings->flags_extend &= ~FLAG_FQPACING
 #define unsetTripTime(settings)     settings->flags_extend &= ~FLAG_TRIPTIME
 #define unsetModeInfinite(settings) settings->flags_extend &= ~FLAG_MODEINFINITE
 #define unsetConnectOnly(settings)  settings->flags_extend &= ~FLAG_CONNECTONLY
-#define unsetWriteAack(settings)    settings->flags_extend &= ~FLAG_WRITEACK
 
 /*
  * Message header flags
@@ -459,6 +459,7 @@ typedef struct thread_Settings {
 #define SEQNO64B              0x00000002
 #define REALTIME              0x00000004
 #define REVERSE               0x00000008
+#define BIDIR                 0x00000010
 
 // later features
 #define HDRXACKMAX 2500000 // default 2.5 seconds, units microseconds
