e0e26030fd7f27150fe8ab0f51863fc91129475a
openvela-robot <openvela-robot@xiaomi.com>
2019-09-12 14:36:25 +0800

    thread startup on bidir

--- src/Reporter.c
+++ src/Reporter.c
@@ -224,7 +224,6 @@ MultiHeader* InitBiDirReport(thread_Settings *agent, int inID) {
 #endif
         agent->bidirhdr = multihdr;
 	multihdr->groupID = inID;
-	multihdr->refcount = 2;
 	if (isMultipleReport(agent)) {
 	    ReporterData *data = &multihdr->report;
 	    data->type = TRANSFER_REPORT;
@@ -259,7 +258,7 @@ MultiHeader* InitBiDirReport(thread_Settings *agent, int inID) {
 	    }
 	}
     } else {
-	FAIL(1, "Out of Memory!!\n", agent);
+            FAIL(1, "Out of Memory!!\n", agent);
     }
     return multihdr;
 }
@@ -313,19 +312,19 @@ static void free_packetring(PacketRing *pr) {
 }
 
 void UpdateMultiHdrRefCounter(MultiHeader *multihdr, int val) {
-    if (!multihdr)
-	return;
-    // decrease the reference counter for mutliheaders
-    // and check to free the multiheader
-    Mutex_Lock( &groupCond );
-    if (multihdr) {
+  if (!multihdr)
+    return;
+  // decrease the reference counter for mutliheaders
+  // and check to free the multiheader
+  Mutex_Lock( &groupCond );
+  if (multihdr) {
 #ifdef HAVE_THREAD_DEBUG
-	thread_debug("Sum multiheader %p ref=%d->%d", (void *)multihdr, \
-		     multihdr->refcount, (multihdr->refcount + val));
+    thread_debug("Sum multiheader %p ref=%d->%d", (void *)multihdr, \
+		 multihdr->refcount, (multihdr->refcount + val));
 #endif
-	multihdr->refcount += val;
-    }
-    Mutex_Unlock( &groupCond );
+    multihdr->refcount += val;
+  }
+  Mutex_Unlock( &groupCond );
 }
 void FreeReport(ReportHeader *reporthdr) {
     if (reporthdr) {
@@ -361,9 +360,9 @@ void InitDataReport(thread_Settings *mSettings) {
 	reporthdr->multireport = mSettings->multihdr;
 	reporthdr->bidirreport = mSettings->bidirhdr;
 	if (reporthdr->bidirreport)
-	    reporthdr->bidirreport->report.info.transferID = mSettings->mSock;
+	  reporthdr->bidirreport->report.info.transferID = mSettings->mSock;
 #ifdef HAVE_THREAD_DEBUG
-	thread_debug("Job report %p uses multireport %p and bidirreport is %p", (void *)mSettings->reporthdr, (void *)reporthdr->multireport, (void *)reporthdr->bidirreport);
+	thread_debug("Multireport is %p, Bidirreport is %p", (void *)reporthdr->multireport, (void *)reporthdr->bidirreport);
 #endif
 	data = &reporthdr->report;
 	data->mThreadMode = mSettings->mThreadMode;
@@ -403,7 +402,7 @@ void InitDataReport(thread_Settings *mSettings) {
 		    reporthdr->output_bidir_handler = output_transfer_bidir_report_tcp;
 		}
 		if (!isServerReverse(mSettings) && (reporthdr->multireport)) {
-		    UpdateMultiHdrRefCounter(reporthdr->multireport, 1);
+		  UpdateMultiHdrRefCounter(reporthdr->multireport, 1);
 		}
 		break;
 	    case kMode_Unknown :
