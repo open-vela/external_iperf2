afd5c524333c23cf040f4b9b152fc9c0c9371e94
openvela-robot <openvela-robot@xiaomi.com>
2020-10-18 11:50:12 +0800

    bump release note date

--- compat/signal.c
+++ compat/signal.c
@@ -52,7 +52,6 @@
 
 #include "headers.h"
 #include "util.h"
-#include "Thread.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -169,15 +168,20 @@ SigfuncPtr my_signal( int inSigno, SigfuncPtr inFunc ) {
  * ------------------------------------------------------------------- */
 
 void sig_exit( int inSigno ) {
-#ifndef HAVE_THREAD
     static int num = 0;
-    if ( num++ == 0 )
-#endif
-    {
+    if ( num++ == 0 ) {
         fflush( 0 );
-        exit(0);
+	_exit(0);
     }
 } /* end sig_exit */
+
+void disarm_itimer(void) {
+#ifdef HAVE_SETITIMER
+    struct itimerval it;
+    memset (&it, 0, sizeof (it));
+    setitimer(ITIMER_REAL, &it, NULL);
+#endif
+}
 
 #ifdef __cplusplus
 } /* end extern "C" */
