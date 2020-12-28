75205b77fa0266c795da9ff776d66a3efc63c55c
openvela-robot <openvela-robot@xiaomi.com>
2020-12-28 12:14:12 +0800

    more on --permit-key

--- include/PerfSocket.hpp
+++ include/PerfSocket.hpp
@@ -59,22 +59,8 @@
 #include "Mutex.h"
 #include "Settings.hpp"
 
-    void SetSocketOptions( thread_Settings *inSettings );
-
-    void SetSocketOptionsSendTimeout( thread_Settings *inSettings, int timer);
-
-    // handle interupts
-    void Sig_Interupt( int inSigno );
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-    extern int sInterupted;
-    extern int groupID;
-    extern Mutex groupCond;
-
-#ifdef __cplusplus
-} /* end extern "C" */
-#endif
+void SetSocketOptions( struct thread_Settings *inSettings );
+void SetSocketOptionsSendTimeout( struct thread_Settings *inSettings, int timer);
+void SetSocketOptionsReceiveTimeout( struct thread_Settings *inSettings, int timer);
 
 #endif // PERFSOCKET_H
