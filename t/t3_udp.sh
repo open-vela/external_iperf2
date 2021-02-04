bd004d327497b62b30f3f3f4e6833ff81e8913d4
liujinye <liujinye@xiaomi.com>
2021-02-04 11:02:23 +0800

    Add igmp_querier to noinst_PROGRAMS so automake can track it.

--- t/t3_udp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-. $(dirname $0)/base.sh
-
-iperf -s -P 1 -u -i 1 -t 3 &
-sleep 0.5
-iperf -c $ip -P 1 -u -b 10m -i 1 -t 2
-wait
-
--- t/t4_udp6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-. $(dirname $0)/base.sh
-
-iperf -V -s -P 1 -u -i 1 -t 3 &
-sleep 0.5
-iperf -V -c $ip6 -P 1 -u -b 10m -i 1 -t 2
-wait
-
