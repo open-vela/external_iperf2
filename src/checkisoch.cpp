cdd63c37ee468a03a1682c3cef48492eafeff49c
openvela-robot <openvela-robot@xiaomi.com>
2021-04-06 20:39:46 +0800

    Merge branch '2-1-1-dev' of ssh://git.code.sf.net/p/iperf2/code into 2-1-1-dev

--- src/checkisoch.cpp
+++ src/checkisoch.cpp
@@ -64,13 +64,14 @@
 static void posttimestamp(int, int);
 
 int main (int argc, char **argv) {
-    int c, count=61, frequency=60;
+    int c, count=100, frequency=100;
     float mean=1e8;
     float variance=3e7;
+    bool forceslip = false;
 
     Isochronous::FrameCounter *fc = NULL;
     
-    while ((c = getopt(argc, argv, "c:f:m:v:")) != -1) {
+    while ((c = getopt(argc, argv, "c:f:m:sv:")) != -1) {
         switch (c) {
         case 'c':
             count = atoi(optarg);
@@ -80,6 +81,9 @@ int main (int argc, char **argv) {
             break;
 	case 'm':
 	    mean=byte_atof(optarg);
+	    break;
+	case 's':
+	    forceslip = true;
 	    break;
 	case 'v':
 	    variance=byte_atof(optarg);
@@ -96,7 +100,7 @@ int main (int argc, char **argv) {
     fprintf(stdout,"Timestamping %d times at %d fps\n", count, frequency);
     fflush(stdout);
     while (count-- > 0) {
-	if (count == 8) {
+	if (forceslip && count == 8) {
 	    delay_loop (1000000/frequency + 10);
 	}
 	fc->wait_tick();
@@ -106,6 +110,7 @@ int main (int argc, char **argv) {
 	    fc->slip = 0;
 	}
     }
+    DELETE_PTR(fc);
 }
 
 void posttimestamp (int count, int bytes) {
