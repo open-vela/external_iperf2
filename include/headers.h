52c7724fbbf7eb71d66103f504ef8582f1e7691d
openvela-robot <openvela-robot@xiaomi.com>
2021-12-28 21:26:56 +0800

    fix: app: iperf2 and wchat pay redefinition
    Fix iperf2/src/Locale.c.c and libwxcodepay.a 'version' redefinition.
    
    Signed-off-by: liufanmin <liufanmin@xiaomi.com>

--- include/headers.h
+++ include/headers.h
@@ -71,6 +71,9 @@
     #endif
 #endif /* HAVE_CONFIG_H */
 
+/* turn off assert debugging */
+#define NDEBUG
+
 /* standard C headers */
 #include <stdlib.h>
 #include <stdio.h>
@@ -83,34 +86,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <inttypes.h>
-#include <limits.h>
-
-#ifdef HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# ifndef HAVE__BOOL
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  else
-#   define _Bool signed char
-#  endif
-# endif
-# define bool _Bool
-# define false 0
-# define true 1
-# define __bool_true_false_are_defined 1
-#endif
-
-
-#if ((defined HAVE_SSM_MULTICAST) || (defined HAVE_DECL_SO_BINDTODEVICE))  && (defined HAVE_NET_IF_H)
-#include <net/if.h>
-#endif
-
-#if ((HAVE_TUNTAP_TAP) || (HAVE_TUNTAP_TUN))
-#include <linux/if_tun.h>
-#include <sys/ioctl.h>
-#endif
-
+#include <getopt.h>
 
 // AF_PACKET HEADERS
 #if defined(HAVE_LINUX_FILTER_H) && defined(HAVE_AF_PACKET)
@@ -226,15 +202,15 @@ SPECIAL_OSF1_EXTERN_C_START
     #include <netdb.h>
 SPECIAL_OSF1_EXTERN_C_STOP
 #endif
-#if HAVE_NETINET_IN_H
+#ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 SPECIAL_OSF1_EXTERN_C_START
     #include <arpa/inet.h>   /* netinet/in.h must be before this on SunOS */
 SPECIAL_OSF1_EXTERN_C_STOP
 #endif
-#if HAVE_NETINET_TCP_H
-#include <netinet/tcp.h>
-#endif
+
+
 
 #ifdef HAVE_POSIX_THREAD
 #include <pthread.h>
@@ -248,7 +224,7 @@ SPECIAL_OSF1_EXTERN_C_STOP
 #endif
 
 //#ifdef __cplusplus
-    #if HAVE_IPV6
+    #ifdef HAVE_IPV6
         #define REPORT_ADDRLEN (INET6_ADDRSTRLEN + 1)
 typedef struct sockaddr_storage iperf_sockaddr;
     #else
@@ -257,7 +233,40 @@ typedef struct sockaddr_in iperf_sockaddr;
     #endif
 //#endif
 
-// inttypes.h is already included
+// Rationalize stdint definitions and sizeof, thanks to ac_create_stdint_h.m4
+// from the gnu archive
+
+#include <iperf-int.h>
+// Override <stdint.h> PRIdMAX (hack for now, fix this to use <stdint.h> properly)
+#ifdef HAVE_QUAD_SUPPORT
+  #ifdef WIN32
+    #define IPERFdMAX "I64d"
+  #elif defined HAVE_PRINTF_QD
+    #define IPERFdMAX "qd"
+  #else
+    #define IPERFdMAX "lld"
+  #endif
+#else
+  #define IPERFdMAX "d"
+#endif
+
+#ifdef HAVE_QUAD_SUPPORT
+#  ifdef HAVE_INT64_T
+typedef int64_t max_size_t;
+typedef u_int64_t umax_size_t;
+#  else
+typedef long long max_size_t;
+typedef unsigned long long umax_size_t;
+#  endif // INT64
+#else
+#  ifdef HAVE_INT32_T
+typedef int32_t max_size_t;
+typedef u_int32_t umax_size_t;
+#  else
+typedef long max_size_t;
+typedef unsigned long umax_size_t;
+#  endif // INT32
+#endif
 
 #ifdef HAVE_FASTSAMPLING
 #define IPERFTimeFrmt "%4.4f-%4.4f"
@@ -277,10 +286,5 @@ typedef struct sockaddr_in iperf_sockaddr;
     #define SHUT_WR   1
     #define SHUT_RDWR 2
 #endif // SHUT_RD
-
-
-/* Internal debug */
-//#define INITIAL_PACKETID 0x7FFFFF00LL
-//#define SHOW_PACKETID
 
 #endif /* HEADERS_H */
