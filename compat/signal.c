4f875b27bd79e945ed2d7cef10dbe05afaf4064b
openvela-robot <openvela-robot@xiaomi.com>
2022-12-18 23:32:43 +0800

    iperf2/src/main.cpp:234:68: warning: format '%ld' expects argument of type 'long int', but argument 3 has type 'time_t' {aka 'long long int'}
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- compat/signal.c
+++ compat/signal.c
@@ -52,6 +52,7 @@
 
 #include "headers.h"
 #include "util.h"
+#include "Thread.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -168,36 +169,15 @@ SigfuncPtr my_signal( int inSigno, SigfuncPtr inFunc ) {
  * ------------------------------------------------------------------- */
 
 void sig_exit( int inSigno ) {
+#ifndef HAVE_THREAD
     static int num = 0;
-    if ( num++ == 0 ) {
+    if ( num++ == 0 )
+#endif
+    {
         fflush( 0 );
-	_exit(0);
+        exit(0);
     }
 } /* end sig_exit */
-
-void disarm_itimer(void) {
-#ifdef HAVE_SETITIMER
-    struct itimerval it;
-    memset (&it, 0, sizeof (it));
-    setitimer(ITIMER_REAL, &it, NULL);
-#endif
-}
-
-int set_itimer(int usecs) {
-    int err = 0;
-#ifdef HAVE_SETITIMER
-    if (usecs < 0) {
-	WARN(1, "set_itimer value invalid");
-    } else {
-	struct itimerval it;
-	memset (&it, 0, sizeof (it));
-	it.it_value.tv_sec = (int)(usecs / 1000000);
-	it.it_value.tv_usec = (int)(usecs % 1000000);
-	err = setitimer(ITIMER_REAL, &it, NULL);
-    }
-#endif
-    return err;
-}
 
 #ifdef __cplusplus
 } /* end extern "C" */
